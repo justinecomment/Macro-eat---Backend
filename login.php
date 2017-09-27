b6d2eee7ee4f32a4490de339fa7d59d1dd6c6f27
Justine <justinecomment@gmail.com>
2017-09-28 00:20:55 +0200

    + Check if Username exist.

--- login.php
+++ login.php
@@ -8,45 +8,57 @@
     include_once('login.php');
     require_once('db.php');
 
+
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $connection = new PDO("mysql:host=$HOST;dbname=$DBNAME", $USER, $PASS);   
+        $connection = new PDO("mysql:host=$HOST;dbname=$DBNAME", $USER, $PASS);
         $_POST = json_decode(file_get_contents('php://input'), true);
 
         $username = $_POST['username'];
         $password = $_POST['password'];
 
-        if(!empty($_POST['username']) && !empty($_POST['password']) ) {
-            $sql = $connection->prepare("insert into login (username, password) values( '$username', '$password')");
-            $resultat = $sql->execute();
-            echo json_encode($resultat);
+        try{
+            $sql = $connection->query("SELECT username, password FROM login WHERE username = '$username'");
+            if($sql->rowCount()> 0 ){
+                throw new Exception('Username existant', 500);
+            }
+            else{
+                $sql = $connection->prepare("INSERT INTO login (username, password) VALUES('$username', '$password')");
+                $result = $sql->execute();
+                echo json_encode($result);
+            }
         }
-    }
+        catch(Exception $e){
+            header("HTTP/1.1 500 Internal Server Error");
+            echo 'error: '.$e->getMessage();
+        }
+    };
+
 
     if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         $connection = new PDO("mysql:host=$HOST;dbname=$DBNAME", $USER, $PASS);
         $username = $_GET['username'];
         $password = $_GET['password'];
 
-            $sql = $connection->query("SELECT username, password FROM login WHERE username = '$username' && `password`= '$password';");
-            while ($result = $sql->fetch()){
-                $data = array('username' => $result['username'],
-                              'password' => $result['password']);
+        $sql = $connection->query("SELECT username, password FROM login WHERE username = '$username' && `password`= '$password';");
+        while ($result = $sql->fetch()){
+            $data = array('username' => $result['username'],
+                          'password' => $result['password']);
+        }
+        try{
+            if($data['username'] !== $username && $data['password'] !== $password )  {
+                throw new Exception('Login incorrect', 500);
             }
-           
-            try{
-                if($data['username'] !== $username && $data['password'] !== $password )  {
-                    throw new Exception('Login incorrect', 500);
-                }
-
+            else{
                 session_start();
                 $_SESSION['username'] = $username;
                 $_SESSION['password'] = $password;
-                echo 'Successful Login';
+                echo $username;
             }
-            catch(Exception $e){
-               header("HTTP/1.1 500 Internal Server Error");
-               echo 'error: '.$e->getMessage();
-            }
+        }
+        catch(Exception $e){
+            header("HTTP/1.1 500 Internal Server Error");
+            echo 'error: '.$e->getMessage();
+        }
 
     }
 
